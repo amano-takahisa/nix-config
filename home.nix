7eae28f55428f205b829f2c0fb77ec135d2478ea
Takahisa Amano <amano.takahisa@gmail.com>
2025-04-06 15:47:30 +0000

    add packages

--- home.nix
+++ home.nix
@@ -7,15 +7,58 @@
   home.stateVersion = "24.11";
 
   home.packages = with pkgs; [
-    # neovim
-    # git
-    htop
+    ##### basic tool #####
+    bat
+    eza
+    fd
+    fish
     fzf
+    htop
+    less
+    libsixel
+    nodejs
+    sshfs
+    which
+    zip
+
+    ##### Neovim #####
+    eslint
+    neovim
+    nodePackages.prettier
+    python3
+    python3Packages.pynvim
+    tree-sitter
+
+    ##### Python #####
+    isort
+    jq
+    pixi
+    ripgrep
+    ripgrep-all
+    ruff
+
+    ##### git #####
+    gh
+    git
+    lazygit
+
+    ##### misc #####
+    tmux
+    tree
     unzip
-    # tmux
+    wl-clipboard
+    xdg-user-dirs
+    zk
+
+    ##### fonts #####
+    hackgen-nf-font
+    font-awesome
   ];
 
-  programs.neovim.enable = true;
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  }
   programs.git.enable = true;
   programs.zsh.enable = true;
   programs.tmux.enable = true;
