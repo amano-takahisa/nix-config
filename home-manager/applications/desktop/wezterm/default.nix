6244a54d868d56509edb635eae20025033bb7e24
Taka <amano.takahisa@gmail.com>
2025-04-11 12:49:01 +0200

    hide wezterm tab

--- home-manager/applications/desktop/wezterm/default.nix
+++ home-manager/applications/desktop/wezterm/default.nix
@@ -1,10 +1,12 @@
-{ config, pkgs, ... }:
-
-let
-  lib = config.lib;
-in
-
 {
-  home.packages = [pkgs.wezterm];
+  programs.wezterm = {
+    enable = true;
+    extraConfig = ''
+    local wezterm = require 'wezterm';
+    return {
+      font = wezterm.font("JetBrains Mono"),
+      hide_tab_bar_if_only_one_tab = true,
+    }
+    '';
+  };
 }
-
