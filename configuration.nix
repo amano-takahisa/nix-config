d54fb1c42cbd054e47b23b748c049743f3c76d0a
Taka <51368373+amano-takahisa@users.noreply.github.com>
2025-04-20 17:33:24 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -29,34 +29,34 @@
   time.timeZone = "Europe/Vienna";
 
   # Select internationalisation properties.
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    supportedLocales = ["en_US.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8" "de_AT.UTF-8/UTF-8"];
-
-    extraLocaleSettings = {
-      LC_ADDRESS = "de_AT.UTF-8";
-      LC_IDENTIFICATION = "de_AT.UTF-8";
-      LC_MEASUREMENT = "de_AT.UTF-8";
-      LC_MONETARY = "de_AT.UTF-8";
-      LC_NAME = "de_AT.UTF-8";
-      LC_NUMERIC = "de_AT.UTF-8";
-      LC_PAPER = "de_AT.UTF-8";
-      LC_TELEPHONE = "de_AT.UTF-8";
-      LC_TIME = "de_AT.UTF-8";
-    };
-
-  inputMethod = {
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8" "de_AT.UTF-8/UTF-8"];
+ 
+  # inputMethod
+  i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
     fcitx5.plasma6Support = true;
     fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs;[
-      fcitx5-configtool
-      fcitx5-gtk
       fcitx5-mozc
-      # kdePackages.fcitx5-qt
+      fcitx5-gtk
     ];
-    };
+  };
+ 
+  # add /etc/x11/xkb
+  services.xserver.exportConfiguration = true;
+ 
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_AT.UTF-8";
+    LC_IDENTIFICATION = "de_AT.UTF-8";
+    LC_MEASUREMENT = "de_AT.UTF-8";
+    LC_MONETARY = "de_AT.UTF-8";
+    LC_NAME = "de_AT.UTF-8";
+    LC_NUMERIC = "de_AT.UTF-8";
+    LC_PAPER = "de_AT.UTF-8";
+    LC_TELEPHONE = "de_AT.UTF-8";
+    LC_TIME = "de_AT.UTF-8";
   };
 
   # Enable the X11 windowing system.
